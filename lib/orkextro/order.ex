fc2198f2c1c860c304e7110eac78cfb3f8354f3b
jekku <jekripreclaroorlina@gmail.com>
2019-10-24 16:15:43 +0800

    Uses persistent API Key instead of an auth server

--- lib/orkextro/order.ex
+++ lib/orkextro/order.ex
@@ -2,19 +2,20 @@ defmodule Orkextro.Order do
   @moduledoc """
   This module handles delivery orders
   """
-  alias Orkextro.{
-    Auth,
-    ConfigHelper
-  }
+  import Orkextro.ConfigHelper
 
-  defp build_headers(access_token) do
-    [Bearer: access_token, "Content-Type": "application/json"]
+  defp build_headers() do
+    api_key = get_api_key()
+    ["Content-Type": "application/json", "api-key": api_key]
+  end
+
+  defp build_url() do
+    "#{get_endpoint()}/orders"
   end
 
   def create(params) do
-    with {:ok, %{"access_token" => access_token}} <- Auth.get_access_token(),
-         {:ok, %{"id" => order_id}} <- do_create(params, access_token),
-         {:ok, order} <- get(order_id, access_token) do
+    with {:ok, %{"id" => order_id}} <- do_create(params),
+         {:ok, order} <- get(order_id) do
       {:ok, order}
     else
       {:error, error} -> {:error, error}
@@ -22,22 +23,30 @@ defmodule Orkextro.Order do
     end
   end
 
-  def do_create(params, access_token) do
+  def do_create(params) do
     case HTTPoison.post(
-           "#{ConfigHelper.get_endpoint()}/orders",
-           build_headers(access_token),
-           params
+           build_url(),
+           Jason.encode!(params),
+           build_headers()
          ) do
-      {:ok, %{status_code: 400, body: body}} -> {:error, Jason.decode!(body)}
-      {:ok, %{status_code: 201, body: body}} -> {:ok, Jason.decode!(body)}
-      {:error, error} -> {:error, error}
+      {:ok, %{status_code: 201, body: body}} ->
+        {:ok, Jason.decode!(body)}
+
+      {:ok, %{status_code: _not_ok, body: body} = all} ->
+        IO.puts("#{inspect(all)}")
+        {:error, Jason.decode!(body)}
+
+      {:error, error} ->
+        {:error, error}
     end
   end
 
-  def get(order_id, access_token) do
-    case HTTPoison.post("#{ConfigHelper.get_endpoint()}/orders", build_headers(access_token), %{
-           id: order_id
-         }) do
+  def get(order_id) do
+    params = %{
+      id: order_id
+    }
+
+    case HTTPoison.post(build_url(), Jason.encode!(params), build_headers()) do
       {:ok, %{status_code: 400, body: body}} -> {:error, Jason.decode!(body)}
       {:ok, %{status_code: 201, body: body}} -> {:ok, Jason.decode!(body)}
       {:error, error} -> {:error, error}
