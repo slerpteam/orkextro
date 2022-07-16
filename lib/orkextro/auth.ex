2235d678066a6d168a6bc9bd1f4d9ec7f21887fe
Tomasz Wójcikowski <pragmainline@gmail.com>
2022-07-16 21:06:56 +0200

    do not defdelegate

--- lib/orkextro/auth.ex
+++ lib/orkextro/auth.ex
@@ -2,7 +2,9 @@ defmodule Orkextro.Auth do
   @moduledoc """
     Core auth module
   """
-  @api Application.compile_env(:orkextro, :api, Orkextro.Request)
 
-  defdelegate authenticate(username, password), to: @api
+  def authenticate(username, password) do
+    mod = Application.get_env(:orkextro, :api, Orkextro.Request)
+    mod.authenticate(username, password)
+  end
 end
