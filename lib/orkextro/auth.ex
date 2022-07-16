5fb17176d2e7a8553d7c0db0d7a8f0a160f38661
Tomasz Wójcikowski <pragmainline@gmail.com>
2022-07-16 20:16:06 +0200

    fix(chore): Update to elixir 1.11

--- lib/orkextro/auth.ex
+++ lib/orkextro/auth.ex
@@ -2,7 +2,7 @@ defmodule Orkextro.Auth do
   @moduledoc """
     Core auth module
   """
-  @api Application.get_env(:orkextro, :api)
+  @api Application.compile_env(:orkextro, :api)
 
   defdelegate authenticate(username, password), to: @api
 end
